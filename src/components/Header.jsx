b93929a767d3a3d21ba2e34fd47a60cd272e53b2
Mikhail <msl2002@yandex.ru>
2021-11-30 17:33:56 +0300

    Start

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="header">
+        <img src="https://marvel-live.freetls.fastly.net/canvas/2018/11/6ffaa98a51d44357ad91edb25fc8349c" />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://img1.badfon.ru/original/1024x768/9/ee/leyk-luiz-kanada-les-nebo.jpg"></img>
+            </div>
+            <div>
+                AVA
+            </div>
+            <div>
+                MY posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
