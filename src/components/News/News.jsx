25325c09db9c193519dad2da9d96ea29fd6d7434
Mikhail <msl2002@yandex.ru>
2021-12-12 20:38:26 +0300

    add browser route & basic components

---
+++ src/components/News/News.jsx
@@ -0,0 +1,11 @@
+import s from "./News.module.css";
+
+const News = (props) => {
+    return (
+        <div className={s.news__wrapper}>
+            News page
+        </div> 
+    );
+}
+
+export default News;
---
+++ src/components/Settings/Settings.jsx
@@ -0,0 +1,11 @@
+import s from "./Settings.module.css"
+
+const Settings = (props) => {
+    return (
+        <div className={s.settings__wrapper}>
+            My settings
+        </div>
+    );
+}
+
+export default Settings;
