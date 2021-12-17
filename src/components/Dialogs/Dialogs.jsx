ba59ca8aa89ac64c768ca910b569a28736f9aa9a
Mikhail <msl2002@yandex.ru>
2021-12-17 19:42:47 +0300

    dialog page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,7 +3,25 @@ import s from './Dialogs.module.css';
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs__wrapper}>
-            Dialogs
+            <div className={s.dialogs__item}>
+                <div className={s.dialog}>
+                    User 1
+                </div>
+                <div className={s.dialog}>
+                    User 2
+                </div>
+                <div className={s.dialog}>
+                    User 3
+                </div>
+            </div>
+            <div className={s.messages__item}>
+                <div className={s.message}>
+                    Hi
+                </div>
+                <div className={s.message}>
+                    How are u?    
+                </div>
+            </div>
         </div>
     );
 }
