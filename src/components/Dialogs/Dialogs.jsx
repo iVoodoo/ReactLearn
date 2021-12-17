54b8148a8e0f8fc0684571c816c4e4bf65a650ab
Mikhail <msl2002@yandex.ru>
2021-12-17 22:48:32 +0300

    refactor
    refactor dialog and profile pages

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,26 +1,32 @@
+import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
+
+const DialogItem = (props) => {
+    return(
+        <div className={s.dialog}>
+            <NavLink to={"/dialogs/"+props.id} className={s.dialog__person}>{props.name}</NavLink> 
+        </div>
+    );
+}
+const Message = (props) => {
+    return(
+        <div className={s.message}>{props.message}</div>
+    );
+}
 
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs__wrapper}>
             <div className={s.dialogs__item}>
-                <div className={s.dialog}>
-                    User 1
-                </div>
-                <div className={s.dialog}>
-                    User 2
-                </div>
-                <div className={s.dialog}>
-                    User 3
-                </div>
+                <DialogItem name="user 1" id="1"/>
+                <DialogItem name="user 2" id="2"/>
+                <DialogItem name="user 3" id="3"/>
+                <DialogItem name="user 4" id="4"/>
+                <DialogItem name="user 5" id="5"/>
             </div>
             <div className={s.messages__item}>
-                <div className={s.message}>
-                    Hi
-                </div>
-                <div className={s.message}>
-                    How are u?    
-                </div>
+                <Message message="hi"/>
+                <Message message="wassup maaan"/>
             </div>
         </div>
     );
